83b0534bc8c76231e95f749da5d0276ee4507324
Fernandobalbinot <129080108+Fernandobalbinot@users.noreply.github.com>
2023-07-05 11:16:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+var blockImageObject = "";
+
+function new_image()
+{
+  fabric.image.fromURL('AniversarioImagem.png', function(Img) {
+    blockImageObject = Img;
+    blockImageObject.scaleToWidth(700);
+    blockImageObject.scaleToHeight(510);    
+    blockImageObject.set({
+    top:0,
+    left:0
+    });
+    canvas.add(blockImageObject);
+    });
+ }
+
+ function playSound(){
+    x.play();
+}
